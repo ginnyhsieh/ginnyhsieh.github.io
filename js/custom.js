1d888e0df55d319c7fb9fc221cd19ed369ada98f
ginnyhsieh <39607568+ginnyhsieh@users.noreply.github.com>
2019-06-09 20:46:11 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	// // header color change
+
+	// $(window).on("scroll",function(){
+	// 	var scrollDistance = $(window).scrollTop();
+	// 	var $header = $(".js_header");
+	// 	if(scrollDistance > 80){
+	// 		$header.addClass("header_colored");
+	// 	}
+	// 	else{
+	// 		$header.removeClass("header_colored");
+	// 	}
+	// })
+
+
+	// wow plugin initialize
+	new WOW().init();
+})
